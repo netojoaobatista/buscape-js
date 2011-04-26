ecf1fca349053a87a514cddbc9062a0edccd6c59
João Batista Neto <neto.joaobatista@gmail.com>
2011-04-26 14:51:23 -0300

    Correção do parâmetro $args para args Correção da validação dos parâmetros na chamada topProducts Correção da validação do argumento productId

--- BuscapeAPI.js
+++ BuscapeAPI.js
@@ -2,10 +2,10 @@
  * @fileoverview A classe BuscapeAPI foi criada para ajudar no desenvolvimento
  *               de aplicações usando os webservices disponibilizados pela API
  *               do BuscaPé.
- * 
+ *
  * Os métodos desta classe tem os mesmos nomes dos serviços disponibilizados
  * pelo BuscaPé.
- * 
+ *
  * @author João Batista Neto
  * @version 0.0.1
  * @license GNU Lesser General Public License Version 2.1, February 1999
@@ -14,7 +14,7 @@
 /**
  * A cada instância criada deverá ser passado como parâmetro obrigatório o id da
  * aplicação. O Source ID não é obrigatório
- * 
+ *
  * @constructor
  * @param {String}
  *            applicationId
@@ -26,46 +26,46 @@
 BuscapeAPI = function( applicationId , sourceId ) {
 	/**
 	 * Id da aplicação
-	 * 
+	 *
 	 * @type String
 	 */
 	this.applicationId = '';
-	
+
 	/**
 	 * Código do país
-	 * 
+	 *
 	 * @type String
 	 */
 	this.countryCode = 'BR';
-	
+
 	/**
 	 * Ambiente do serviço
-	 * 
+	 *
 	 * @type String
 	 */
 	this.environment = 'bws';
-	
+
 	/**
 	 * Formato de retorno padrão
-	 * 
+	 *
 	 * @type String
 	 */
 	this.format = 'json';
-	
+
 	/**
 	 * Source id
-	 * 
+	 *
 	 * @type String
 	 */
 	this.sourceId = '';
-	
+
 	this.setApplicationId( applicationId );
 	this.setSourceId( sourceId );
 };
 
 /**
  * Recebe a resposta JSON da API do BuscaPé
- * 
+ *
  * @param {Object}
  *            JSON correspondente a resposta da chamada efetuada
  */
@@ -75,7 +75,7 @@ BuscapeAPI.receiveContent = function( result ) {
 
 /**
  * Recebe a resposta JSON da API do BuscaPé
- * 
+ *
  * @param {Object}
  *            JSON correspondente a resposta da chamada efetuada
  */
@@ -84,7 +84,7 @@ BuscapeAPI.prototype.receiveContent = function( result ) {
 
 /**
  * Método busca retorna os dados da url requisitada
- * 
+ *
  * @param {String}
  *            serviceName Nome do serviço
  * @param {Object}
@@ -96,31 +96,37 @@ BuscapeAPI.prototype.receiveContent = function( result ) {
  */
 BuscapeAPI.prototype._getContent = function( serviceName , args , lomadee ) {
 	var params = '';
-	
+
 	if ( lomadee ) {
 		serviceName += '/lomadee';
 	}
-	
+
+	if ( args === undefined ) {
+		args = {};
+	}
+
 	if ( this.sourceId.length > 0 ) {
 		args.sourceId = this.sourceId;
 	}
-	
+
 	if ( this.format == 'json' ) {
 		args.format = this.format;
-		
-		if ( args.callback != undefined ) {
-			args.callback = 'BuscapeAPI.receiveContent';
-		}
-	}
 	
+		if ( args.callback !== undefined ) {
+			this.receiveContent = args.callback;
+		}
+	
+		args.callback = 'BuscapeAPI.receiveContent';
+	}
+
 	for ( var property in args ) {
 		params += [ params.length == 0 ? '?' : '&' , [ property , args[ property ] ].join( '=' ) ].join( '' );
 	}
-	
+
 	var url = [ 'http:/' , [ this.environment , 'buscape.com/service' ].join( '.' ) , serviceName , this.applicationId , this.countryCode , params ].join( '/' );
-	
+
 	BuscapeAPI.bpObject = this;
-	
+
 	document.write( [ '<script type="text/javascript" src=' , url , '></script>' ].join( '"' ) );
 };
 
@@ -132,10 +138,10 @@ BuscapeAPI.prototype._getContent = function( serviceName , args , lomadee ) {
  * de homologação sandbox, os valores dos parâmetros podem ser fictícios pois
  * neste ambiente este serviço retornará sempre o mesmo sourceId para os testes
  * do Developer.
- * 
+ *
  * Todos os parâmetros necessários para a busca são informados em um objeto que
  * deve ser passado para o método, são eles:
- * 
+ *
  * <ul>
  * <li>sourceName = Nome do código.</li>
  * <li>publisherId = ID do publisher.</li>
@@ -143,7 +149,7 @@ BuscapeAPI.prototype._getContent = function( serviceName , args , lomadee ) {
  * <li>campaignList = Lista de IDs das campanhas separados por vírgula.</li>
  * <li>token = Token utilizado para validação da requisição.</li>
  * </ul>
- * 
+ *
  * @param {Object}
  *            args
  * @return {String} O sourceId
@@ -156,18 +162,18 @@ BuscapeAPI.prototype.createSourceId = function( args ) {
  * Método faz busca de categorias, permite que você exiba informações relativas
  * às categorias. É possível obter categorias, produtos ou ofertas informando
  * apenas um ID de categoria.
- * 
- * 
+ *
+ *
  * <ul>
  * <li>categoryId = Id da categoria</li>
  * <li>keyword = Palavra-chave buscada entre as categorias</li>
  * <li>callback = Função de retorno a ser executada caso esteja usando o método
  * json como retorno.</li>
  * </ul>
- * 
+ *
  * Se não for informado nenhum dos parâmetros, a função retornará por padrão uma
  * lista de categorias raiz, de id 0.
- * 
+ *
  * @param {Object}
  *            args Parâmetros passados para gerar a url de requisição
  * @return {String} Retorno da pesquisa feita no BuscaPé, no formato requerido.
@@ -185,10 +191,10 @@ BuscapeAPI.prototype.findCategoryList = function( args ) {
 /**
  * Método busca uma lista de ofertas. É possível obter a lista de ofertas
  * informando o ID do produto.
- * 
+ *
  * Todos os parâmetros necessários para a busca são informados em um array que
  * deve ser passado para o método, são eles:
- * 
+ *
  * <ul>
  * <li>categoryId = Id da categoria</li>
  * <li>keyword = Palavra-chave buscada entre as categorias</li>
@@ -197,11 +203,11 @@ BuscapeAPI.prototype.findCategoryList = function( args ) {
  * <li>callback = Função de retorno a ser executada caso esteja usando o método
  * json como retorno.</li>
  * </ul>
- * 
+ *
  * Pelo menos um dos parâmetros de pesquisa devem ser informados para o retorno
  * da função. Os parâmetros <categoryId> e <keyword> podem ser usados em
  * conjunto.
- * 
+ *
  * @param array
  *            $args Parâmetros passados para gerar a url de requisição.
  * @param boolean
@@ -223,21 +229,21 @@ BuscapeAPI.prototype.findOfferList = function( args , lomadee ) {
 /**
  * Método permite que você busque uma lista de produtos únicos utilizando o id
  * da categoria final ou um conjunto de palavras-chaves ou ambos.
- * 
+ *
  * Todos os parâmetros necessários para a busca são informados em um array que
  * deve ser passado para o método, são eles:
- * 
+ *
  * <ul>
  * <li>categoryId = Id da categoria</li>
  * <li>keyword = Palavra-chave buscada entre as categorias</li>
  * <li>callback = Função de retorno a ser executada caso esteja usando o método
  * json como retorno.</li>
  * </ul>
- * 
+ *
  * Pelo menos um dos parâmetros, <categoryID> ou <keyword> são requeridos para
  * funcionamento desta função. Os dois também podem ser usados em conjunto. Ou
  * seja, podemos buscar uma palavra-chave em apenas uma determinada categoria.
- * 
+ *
  * @param {Object}
  *            args Parâmetros para gerar a url de requisição
  * @param {Boolean}
@@ -256,7 +262,7 @@ BuscapeAPI.prototype.findProductList = function( args , lomadee ) {
 
 /**
  * Retorna o Id da aplicação
- * 
+ *
  * @return {String}
  */
 BuscapeAPI.prototype.getApplicationId = function() {
@@ -265,7 +271,7 @@ BuscapeAPI.prototype.getApplicationId = function() {
 
 /**
  * Retorna o código do país
- * 
+ *
  * @return {String}
  */
 BuscapeAPI.prototype.getCountryCode = function() {
@@ -274,7 +280,7 @@ BuscapeAPI.prototype.getCountryCode = function() {
 
 /**
  * Retorna o ambiente de integração
- * 
+ *
  * @return {String}
  */
 BuscapeAPI.prototype.getEnvironment = function() {
@@ -283,7 +289,7 @@ BuscapeAPI.prototype.getEnvironment = function() {
 
 /**
  * Retorna o formato de retorno
- * 
+ *
  * @return {String}
  */
 BuscapeAPI.prototype.getFormat = function() {
@@ -292,7 +298,7 @@ BuscapeAPI.prototype.getFormat = function() {
 
 /**
  * Retorna o Source ID
- * 
+ *
  * @return {String}
  */
 BuscapeAPI.prototype.getSourceId = function() {
@@ -301,7 +307,7 @@ BuscapeAPI.prototype.getSourceId = function() {
 
 /**
  * Define o Id da aplicação
- * 
+ *
  * @param {String}
  *            applicationId O id da aplicação
  * @throws Caso
@@ -309,7 +315,7 @@ BuscapeAPI.prototype.getSourceId = function() {
  */
 BuscapeAPI.prototype.setApplicationId = function( applicationId ) {
 	applicationId = applicationId + '';
-	
+
 	if ( applicationId.split( ' ' ).join( '' ).length == 0 ) {
 		throw 'Id da aplicação inválido';
 	}
@@ -320,7 +326,7 @@ BuscapeAPI.prototype.setApplicationId = function( applicationId ) {
 
 /**
  * Define o código do país
- * 
+ *
  * @param {String}
  *            countryCode (AR|BR|CL|CO|MX|PE|VE)
  * @throws Se
@@ -344,7 +350,7 @@ BuscapeAPI.prototype.setCountryCode = function( countryCode ) {
 
 /**
  * Define o formato de retorno
- * 
+ *
  * @param {String}
  *            format (xml|json)
  * @throws Se
@@ -370,7 +376,7 @@ BuscapeAPI.prototype.setSandbox = function() {
 
 /**
  * Define o Source Id
- * 
+ *
  * @param {String}
  *            sourceId
  */
@@ -380,15 +386,15 @@ BuscapeAPI.prototype.setSourceId = function( sourceId ) {
 
 /**
  * Método retorna os produtos mais populares do BuscaPé
- * 
+ *
  * Todos os parâmetros necessários para a busca são informados em um array que
  * deve ser passado para o método, são eles:
- * 
+ *
  * <ul>
  * <li>callback = Função de retorno a ser executada caso esteja usando o método
  * json como retorno.</li>
  * </ul>
- * 
+ *
  * @param array
  *            $args Parâmetros passados para gerar a url de requisição.
  * @return string Retorno da pesquisa feita no BuscaPé, no formato requerido.
@@ -396,12 +402,12 @@ BuscapeAPI.prototype.setSourceId = function( sourceId ) {
  *             Se o id da categoria for menor que zero.
  */
 BuscapeAPI.prototype.topProducts = function( args ) {
-	return this._getContent( 'topProducts' , this.validateParams( $args ) );
+	return this._getContent( 'topProducts' , this.validateParams( args ) );
 };
 
 /**
  * Valida os parâmetros que serão enviados à API do BuscaPé
- * 
+ *
  * @param {Object}
  *            args Matriz com os parâmetros
  * @param {Array}
@@ -418,73 +424,77 @@ BuscapeAPI.prototype.topProducts = function( args ) {
  */
 BuscapeAPI.prototype.validateParams = function( args , and , or ) {
 	if ( args instanceof Object ) {
-		if ( args.keyword != undefined ) {
+		if ( args.keyword !== undefined ) {
 			args.keyword = [ args.keyword ].join( '' ).split( ' ' ).join( '' );
-			
+		
 			if ( args.keyword.length == 0 ) {
 				throw 'A palavra chave não pode ser uma string vazia';
 			}
 		}
-		
-		if ( args.categoryId != undefined ) {
+	
+		if ( args.categoryId !== undefined ) {
 			args.categoryId = args.categoryId instanceof Number ? args.categoryId : parseInt( args.categoryId );
-			
+		
 			if ( isNaN( args.categoryId ) || args.categoryId < 0 ) {
 				throw 'O id da categoria deve ser um número maior ou igual a zero';
 			}
 		}
-		
-		if ( args.productId != undefined ) {
+	
+		if ( args.productId !== undefined ) {
 			args.productId = args.productId instanceof Number ? args.productId : parseInt( args.productId );
-			
-			if ( isNaN( args.categoryId ) || args.categoryId < 0 ) {
+		
+			if ( isNaN( args.productId ) || args.productId < 0 ) {
 				throw 'O id do produto deve ser um número maior ou igual a zero';
 			}
 		}
-		
-		if ( args.sellerId != undefined ) {
+	
+		if ( args.sellerId !== undefined ) {
 			args.sellerId = args.sellerId instanceof Number ? args.sellerId : parseInt( args.sellerId );
-			
+		
 			if ( isNaN( args.sellerId ) || args.sellerId < 0 ) {
 				throw 'O id da loja/empresa deve ser um número maior ou igual a zero';
 			}
 		}
-		
-		var i , t , f;
-		
-		for ( i = 0 , t = and.length ; i < t ; ++i ) {
-			if ( args[ and[ i ] ] == undefined ) {
-				throw [ 'O parâmetro' , and[ i ] , 'é requerido' ].join( ' ' );
+	
+		var i, t, f;
+	
+		if ( and !== undefined ) {
+			for ( i = 0 , t = and.length ; i < t ; ++i ) {
+				if ( args[ and[ i ] ] === undefined ) {
+					throw [ 'O parâmetro' , and[ i ] , 'é requerido' ].join( ' ' );
+				}
 			}
 		}
+	
+		if ( or !== undefined ) {
+			for ( i = 0 , t = or.length , f = 0 ; i < t ; ++i ) {
+				if ( args[ or[ i ] ] !== undefined ) {
+					++f;
+					break;
+				}
+			}
 		
-		for ( i = 0 , t = or.length , f = 0 ; i < t ; ++i ) {
-			if ( args[ or[ i ] ] != undefined ) {
-				++f;
-				break;
+			if ( t != 0 && f == 0 ) {
+				throw [ 'Pelo menos um dos parâmetros: "' , or.join( '","' ) , '" devem ser passados' ].join( '' );
 			}
 		}
-		
-		if ( t != 0 && f == 0 ) {
-			throw [ 'Pelo menos um dos parâmetros: "' , or.join( '","' ) , '" devem ser passados' ].join( '' );
-		}
-		
+	
 		return args;
 	}
 };
 
 /**
  * Função retorna os detalhes técnicos de um determinado produto.
- * 
+ *
  * Todos os parâmetros necessários para a busca são informados em um array que
  * deve ser passado para o método, são eles:
- * 
+ *
  * <ul>
  * <li>productId = Id do produto (requerido)</li>
  * <li>callback = Função de retorno a ser executada caso esteja usando o método
  * json como retorno.</li>
  * </ul>
- * 
+ *
  * @param {Object}
  *            args Parâmetros passados para gerar a url de requisição.
  * @return {String} Função de retorno a ser executada caso esteja usando o
@@ -499,16 +509,16 @@ BuscapeAPI.prototype.viewProductDetails = function( args ) {
 /**
  * Função retorna os detalhes da loja/empresa como: endereços, telefones de
  * contato etc...
- * 
+ *
  * Todos os parâmetros necessários para a busca são informados em um array que
  * deve ser passado para o método, são eles:
- * 
+ *
  * <ul>
  * <li>sallerId = Id da loja/empresa (requerido)</li>
  * <li>callback = Função de retorno a ser executada caso esteja usando o método
  * json como retorno.</li>
  * </ul>
- * 
+ *
  * @param {Object}
  *            args Parâmetros passados para gerar a url de requisição.
  * @return {String} Função de retorno a ser executada caso esteja usando o
@@ -522,16 +532,16 @@ BuscapeAPI.prototype.viewSellerDetails = function( args ) {
 
 /**
  * Método retorna as avaliações dos usuários sobre um determinado produto
- * 
+ *
  * Todos os parâmetros necessários para a busca são informados em um array que
  * deve ser passado para o método, são eles:
- * 
+ *
  * <ul>
  * <li>productId = Id do produto (requerido)</li>
  * <li>callback = Função de retorno a ser executada caso esteja usando o método
  * json como retorno.</li>
  * </ul>
- * 
+ *
  * @param {Object}
  *            args Parâmetros passados para gerar a url de requisição.
  * @return {String} Retorno da pesquisa feita no BuscaPé, no formato requerido.
